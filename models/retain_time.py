18643eaaab81e9606093fd58d7b818fb6483955a
Min-je Choi <devnote5676@naver.com>
2018-03-03 01:09:59 +0900

    Added bidirectional mode

--- models/retain_time.py
+++ models/retain_time.py
@@ -1,27 +1,38 @@
 import torch
 from torch import nn
+import numpy as np
 from torch.autograd import Variable
 import torch.nn.functional as F
 import time
 
 class RETAIN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes, cuda_flag=False):
+    def __init__(self, input_size, hidden_size, num_classes,
+        cuda_flag=False, bidirectional=False):
         super(RETAIN,self).__init__()
         self.hidden_size = hidden_size
         self.input_size = input_size
         self.cuda_flag = cuda_flag
         self.release = False # if set to true, then we return all values in computing
+        self.bidirectional = bidirectional
+
         emb = nn.Embedding(1400,input_size)
         self.emb = emb.weight
 
         self.RNN1 = TimeLSTM(input_size,hidden_size,cuda_flag)
         self.RNN2 = TimeLSTM(input_size,hidden_size,cuda_flag)
+        if self.bidirectional:
+            self.RNN1b = TimeLSTM(input_size,hidden_size,cuda_flag)
+            self.RNN2b = TimeLSTM(input_size,hidden_size,cuda_flag)
         if cuda_flag:
             self.RNN1.cuda()
             self.RNN2.cuda()
 
         self.wa = nn.Linear(hidden_size,1,bias=False)
         self.Wb = nn.Linear(hidden_size,hidden_size,bias=False)
+        if self.bidirectional:
+            self.wa = nn.Linear(hidden_size*2,1,bias=False)
+            self.Wb = nn.Linear(hidden_size*2,hidden_size,bias=False)
+
         self.W_out = nn.Linear(hidden_size,num_classes,bias=False)
 
     def forward(self, inputs, timestamps):
@@ -33,6 +44,21 @@ class RETAIN(nn.Module):
 
         # get alpha coefficients
         outputs1 = self.RNN1(embedded, timestamps) # [b x seq x 128]
+        outputs2 = self.RNN2(embedded, timestamps) # [b x seq x 128]
+        if self.bidirectional:
+            # add version where reverse is taken in
+            embedded_b = embedded.data.cpu().numpy()
+            embedded_b = Variable(torch.Tensor(np.flip(embedded_b,1)))
+            timestamps_b = timestamps.data.cpu().numpy()
+            timestamps_b = Variable(torch.Tensor(np.flip(timestamps_b,1)))
+            if self.cuda_flag:
+                embedded_b = embedded_b.cuda()
+                timestamps_b = timestamps_b.cuda()
+            outputs1b = self.RNN1b(embedded_b, timestamps_b, reverse=True)
+            outputs2b = self.RNN2b(embedded_b, timestamps_b, reverse=True)
+            outputs1 = torch.stack([outputs1,outputs1b],2)
+            outputs2 = torch.stack([outputs2,outputs2b],2)
+
         E = self.wa(outputs1.contiguous().view(-1, self.hidden_size)) # [b*seq x 1]
         alpha = F.softmax(E.view(b,seq),1) # [b x seq]
         if self.release:
@@ -81,7 +107,7 @@ class RETAIN(nn.Module):
         return out
 
 class TimeLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, cuda_flag=False):
+    def __init__(self, input_size, hidden_size, cuda_flag=False, bidirectional=False):
         # assumes that batch_first is always true
         super(TimeLSTM,self).__init__()
         self.hidden_size = hidden_size
@@ -90,43 +116,34 @@ class TimeLSTM(nn.Module):
         self.W_all = nn.Linear(input_size, hidden_size*4)
         self.U_all = nn.Linear(hidden_size, hidden_size*4)
         self.W_d = nn.Linear(hidden_size, hidden_size)
+        self.bidirectional = bidirectional
 
-    def forward(self, inputs, timestamps):
+    def forward(self, inputs, timestamps, reverse=False):
         # inputs: [b, seq, hid]
         # h: [b, hid]
         # c: [b, hid]
         b,seq,hid = inputs.size()
-        # print("inputs: ",inputs[0,:10])
-        h = Variable(torch.randn(b,hid), requires_grad=False)
-        c = Variable(torch.randn(b,hid), requires_grad=False)
+        h = Variable(torch.Tensor(b,hid).zero_(), requires_grad=False)
+        c = Variable(torch.randn(b,hid).zero_(), requires_grad=False)
         if self.cuda_flag:
             h = h.cuda()
             c = c.cuda()
         outputs = []
-        # print("W_all: ",self.W_all.weight[0,:10])
         for s in range(seq):
-            # c_s1: [b, hid]
-            # c_s2: [b, hid]
-            # c_l: [b, hid]
-            # c_adj: [b, hid]
             c_s1 = F.tanh(self.W_d(c))
             c_s2 = c_s1 * timestamps[:,s:s+1].expand_as(c_s1)
             c_l = c - c_s1
             c_adj = c_l + c_s2
-            # print('c_adj: ',c_adj[0,:10])
             outs = self.W_all(h)+self.U_all(inputs[:,s])
-            # print(outs.size())
             f, i, o, c_tmp = torch.chunk(outs,4,1)
             f = F.sigmoid(f)
             i = F.sigmoid(i)
             o = F.sigmoid(o)
             c_tmp = F.sigmoid(c_tmp)
-            # print('c_adj: ',c_adj.size())
-            # print('f: ',f.size())
-            # print('i: ',i.size())
-            # print('c_tmp: ',c_tmp.size())
             c = f*c_adj + i*c_tmp
             h = o*F.tanh(c)
             outputs.append(h)
+        if reverse:
+            outputs.reverse()
         outputs = torch.stack(outputs,1)
         return outputs
